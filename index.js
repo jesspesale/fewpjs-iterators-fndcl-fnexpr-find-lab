4217b20c3e92785c294d37eda28fdc598ae6e248
Jess Pesale <jess_pesale@aol.com>
2021-08-18 14:52:43 -0400

    commit tree e750b80dd032144d8c6d31b9f5554b8a08a6f412

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+function superbowlWin(array) {
+    const winner = array.find( win => win.result === "W")
+    if  (winner) {
+        return winner.year
+    }
+    else {
+        return undefined
+    }
+}
+
+
+
